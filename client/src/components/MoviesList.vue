ce06ba1e7e065735f8f854c04b1a80e2f455a0ab
alukardv <semenovnsu@gmail.com>
2022-05-12 23:57:48 +0300

    dz11_6

--- client/src/components/MoviesList.vue
+++ client/src/components/MoviesList.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class='container'>
   <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 g-3">
@@ -6,7 +5,7 @@
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
                  preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect>
-              <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+              <text x="35%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
             <div class="card-body">
               <a :href="`/movie/${movie.id}/`" class="movie-list-item">
@@ -19,7 +18,7 @@
           </div>
         </div>
       </div>
-
+<hr>
       <nav aria-label="Page navigation example">
         <ul class="pagination">
 
